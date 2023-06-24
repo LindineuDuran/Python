b66e5d457344c5b2085d5b6a4b810dba0b8a5bbb
Lindineu Lopes Duran <37485332+LindineuDuran@users.noreply.github.com>
2023-06-24 18:33:50 -0300

    Atualizado o teste_youtube
    Atualizado o teste_youtube que utiliza a lib yt_dlp

--- Download Youtube Videos/teste_youtube.py
+++ Download Youtube Videos/teste_youtube.py
@@ -1,62 +1,33 @@
-import sys
 import os
 import subprocess
-import json
 import yt_dlp
 
 app_path = os.path.dirname(os.path.abspath(__file__))
 ffmpeg = os.path.join(app_path, 'ffmpeg.exe')
 
-# links = 'https://www.youtube.com/watch?v=30VHcPI6sDs&t=1921s&ab_channel=AurioCorr%C3%A1'
-# ydl_opts = {'writesubtitles': True, 'skip-download': True}
-# with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-#     ydl.download([links])
-
 # yt-dlp --extract-audio --audio-format mp3 --audio-quality 0 "https://www.youtube.com/watch?v=oHg5SJYRHA0"
-links = 'https://www.youtube.com/watch?v=jku4qn0Mchg&t=122s&ab_channel=AurioCorr%C3%A1'
+links = 'https://www.youtube.com/watch?v=XKZw2UtWPqA&ab_channel=AurioCorr%C3%A1'
 ydl_opts = {'ignoreerrors': True, 'extract-audio': True, 'audio-format': 'mp3', 'audio-quality' : 0}
 
 audio_name = None
+id = None
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
     ydl.download([links])
     info = ydl.extract_info(links)
     audio_name = info.get('title')
+    id = info.get('id')
 
-    # info = ydl.extract_info(links, download=False)
-    
-    # # ℹ️ ydl.sanitize_info makes the info json-serializable
-    #print(json.dumps(ydl.sanitize_info(info)))
-    # print('=================================')
-    #print(info.title)
-
-#audio_name = 'THE LADIES OF  AVALON - CELTIC MUSIC, ELEVAR VIBRAÇÕES DA EMOÇÃO,  DO AMOR,  DA CURA,  E A HARMONIA. [jku4qn0Mchg].f251.webm'
-
-import subprocess
-someFilename = subprocess.getoutput('yt-dlp --print filename https://www.youtube.com/something')
-# then pass someFilename to FFmpeg
-
-subprocess.run('yt-dlp -o thisIsMyName https://www.youtube.com/something')
-# this will likely download a file named thisIsMyName.webm
-
-someFileType = subprocess.getoutput('yt-dlp --print filename -o "%(ext)s" https://www.youtube.com/something')
-print(someFileType)
-
-source = os.path.join(app_path, audio_name)
+source = os.path.join(app_path, audio_name + ' [' + id + ']' +'.webm')
 
 if os.path.isfile(source.replace(' ', '_')):
     os.remove(source.replace(' ', '_'))
 
-# if ' ' in source:
-#     os.rename(source, source.replace(' ', '_'))
+if ' ' in source:
+    os.rename(source, source.replace(' ', '_'))
 
 audio_name = audio_name.replace(' ', '_')
 source = source.replace(' ', '_')
 
 file_without_ext = os.path.splitext(source)[0]
 
-print("source: " + source)
-print("final: " + os.path.join(app_path, file_without_ext + '.mp3'))
-
-result = subprocess.run(['ffmpeg.exe', '-y', '-i', source, os.path.join(
-    app_path, file_without_ext + '.mp3')], shell=True, check=True)
-
+result = subprocess.run(['ffmpeg.exe', '-y', '-i', source, os.path.join(app_path, file_without_ext + '.mp3')], shell=True, check=True)
